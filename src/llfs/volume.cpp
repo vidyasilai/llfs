3922c94810f3ef19895a974276b64722053c38de
Vidya Silai <vsilai@umich.edu>
2026-06-04 11:29:31 -0400

    Add print debugging in ~Volume

--- src/llfs/volume.cpp
+++ src/llfs/volume.cpp
@@ -369,11 +369,16 @@ u64 Volume::calculate_grant_size(const AppendableJob& appendable) const
 //==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
 //
 Volume::~Volume() noexcept
-{
-  this->pre_halt();
-  this->root_log_->flush().IgnoreError();
-  this->halt();
-  this->join();
+{                                                                                                                    
+  LOG(INFO) << "~Volume: pre_halt...";
+  this->pre_halt();                                                                                                  
+  LOG(INFO) << "~Volume: flush...";                                                                                  
+  this->root_log_->flush().IgnoreError();                                                                            
+  LOG(INFO) << "~Volume: halt...";                                                                                   
+  this->halt();                                                                                                      
+  LOG(INFO) << "~Volume: join...";                                                                                   
+  this->join();                                                                                                      
+  LOG(INFO) << "~Volume: done";                                                                                      
 }
 
 //==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
